b1254753105b2a6c0690840377acd79e9f266e1e
Syed Ghazi Ali Zaidi <sghazializaidi@gmail.com>
2024-01-23 23:54:33 +0500

    Add project files.

---
+++ Jobify/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jobify.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpPost]
+        public string CategoryPost(string name)
+        {
+            return "Your name is "+name;
+        }
+
+        [HttpGet]
+        public string CategoryGet()
+        {
+            return "Category Price 100";
+        }
+    }
+}
